59f0b9cbde1715c4b880e7e279344b381f638955
NgocVT20 <ngocvt20@fpt.com>
2023-09-06 13:52:46 +0700

    authen

--- src/main/java/com/example/springsecurity/Security/WebSecurityConfig.java
+++ src/main/java/com/example/springsecurity/Security/WebSecurityConfig.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,5 +39,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         // mã hóa mật khẩu người dùng
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService) // Cung cap customUserDetailsService cho spring security
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and().csrf().disable() //Ngăn chặn request từ domain khác
+                .authorizeRequests()
+                .antMatchers("/api/v1/auth/**").permitAll() // cho phep tatats cả
+                .antMatchers("api/v1/test/**").permitAll()
+                .anyRequest().authenticated(); // các request khác thì cần xác thực
+
+        // thêm 1 lớp filter để kiểm tra jwt
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
